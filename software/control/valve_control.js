ebecb0b680c9775655bc9dcff6c9e9e34d809c85
zackaholic <zackaholic@gmail.com>
2018-03-31 22:47:08 +0000

    adds valve exports

--- software/control/valve_control.js
+++ software/control/valve_control.js
@@ -7,40 +7,15 @@ pwmDriver.setPWMFreq(50);
 const servo_min = 150 // Min pulse length out of 4096
 const servo_max = 600 // Max pulse length out of 4096
 
-const setPosition = (pulse, setFunction) => {
-	return (channel) => {
-		setFunction(channel, 0, pulse);
-	}
+const setPosition = (channel, pulseDuration, setFunction) => {
+  setFunction(channel, 0,  pulseDuration);
 }
 
-setToggle = (openFun, closeFun) => {
-//to be usefull this has to actually get servo position from the controller
-	let servoState = 1;
-	return (channel) => {
-		if (servoState > 0) {
-			closeFun(channel);
-		} else {
-			openFun(channel);
-		}
-		servoState *= -1;
-	}
-};
-
-const open = setPosition(500, pwmDriver.setPWM);
-const close = setPosition(400, pwmDriver.setPWM);
-const toggle = setToggle(open, close);
-
-if (process.argv[2]){
-  if (process.argv[2] === 'open') {
-    console.log('opening');
-    open(0);
-  } 
-  if (process.argv[2] === 'close') {
-    console.log('closing');
-    close(0);
+const savePosition = (pulse) => {
+  return (channel) => {
+    setPosition.call(null, channel, pulse, pwmDriver.setPWM);
   }
-} 
+}
 
-//setInterval(function() {
-//	toggle(0);
-//}, 1000);
+module.exports.close = savePosition(400);
+module.exports.open = savePosition(500);
