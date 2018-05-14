59648f2a3df8223bf59aace3e4050d8571bb54ed
zackaholic <zackaholic@gmail.com>
2018-05-14 17:44:25 +0000

    move button assignment into app, detach listeners during dispense operation

--- software/control/app.js
+++ software/control/app.js
@@ -1,47 +1,68 @@
 const menu = require('./recipes.js');
 const dispense = require('./dispense.js');
-const input = require('./interface.js');
 const mover = require('./mover.js');
 const logger = require('tracer').console();
 const LED = require('./led_control.js');
+const Gpio = require('onoff').Gpio;
 
-const led1 = new LED(4);
-const led2 = new LED(5);
-const led3 = new LED(6);
-const led4 = new LED(7);
-led1.on();
-led2.on();
-led3.on();
-led4.on();
+const buttons = [
+ new Gpio(17, 'in', 'falling', {debounceTimeout: 20}),
+ new Gpio(27, 'in', 'falling', {debounceTimeout: 20}),
+ new Gpio(22, 'in', 'falling', {debounceTimeout: 20}),
+ new Gpio(10, 'in', 'falling', {debounceTimeout: 20})
+];
+
+const LEDs = [
+ new LED(4),
+ new LED(5),
+ new LED(6),
+ new LED(7),
+];
+
+const LEDsOn = (leds) => {
+  leds.forEach((e) => {
+    e.on();
+  });
+}
+
+const LEDsOff = (leds) => {
+  leds.forEach((e) => {
+    e.stopFlash();
+    e.off();
+  });
+}
 
 process.on('SIGINT', () => {
-  led1.stopFlash();
-  led2.stopFlash();
-  led3.stopFlash();
-  led4.stopFlash();
-  led1.off();
-  led2.off();
-  led3.off();
-  led4.off();
-  console.log('LEDs off');
-});
+  buttons[0].unexport();
+  buttons[1].unexport();
+  buttons[2].unexport();
+  buttons[3].unexport();
+  
+  LEDsOff(LEDs);
 
+  console.log('\nbuttons detached, LEDs off');
+});
 
 const dispenser = (dispenseFunc, moveFunc) => {
   return (recipe, indicator) => {
-    return () => {
+    return (err) => {
+      if (err) {
+        return console.log(err);
+      }
       logger.log(`moving to dispense ${recipe}`);
       indicator.flash(2);
+      unwatchButtons();
       moveFunc()
       .then(() => {
         logger.log('beginning pour');
-        return dispenseFunc(recipe);
+        return dispenseFunc(menu.recipe(recipe));
       })
       .then(() => {
         logger.log('dispense complete, attract call');
         indicator.stopFlash();
         indicator.on();
 //        mover.attract();
+        watchButtons();
       })
       .catch((err) => {
         logger.log(err);
@@ -52,14 +73,26 @@ const dispenser = (dispenseFunc, moveFunc) => {
 
 const dispenseDrink = dispenser(dispense.dispenseSequentialP, mover.dispenseP);
 
-input.assign([
-  dispenseDrink(menu.recipe(menu.list()[0]), led1),
-  dispenseDrink(menu.recipe(menu.list()[1]), led2),
-  dispenseDrink(menu.recipe(menu.list()[2]), led3),
-  dispenseDrink(menu.recipe(menu.list()[3]), led4)
-]);
+
+const watchButtons = ((buttons, dispenseFn, menu, LEDs) => {
+  return () => {
+    buttons.forEach((butt, i) => {
+      butt.watch(dispenseFn(menu[i], LEDs[i]));
+    });
+  }
+})(buttons, dispenseDrink, menu.list(), LEDs);
+
+const unwatchButtons  = ((buttons) => {
+  return () => {
+    buttons.forEach((e) => {
+      e.unwatchAll();
+    });
+  };
+})(buttons);
 
 const start = () => {
+  LEDsOn(LEDs);
+  watchButtons();
   mover.setSpeed(10000);
 //  mover.attract();
 }
